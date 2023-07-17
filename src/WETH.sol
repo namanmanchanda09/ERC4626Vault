1079a61c2c735bf4b2029ff4a7fff2146d2515e5
Naman Manchanda <naman.mohan09@outlook.com>
2023-07-17 13:06:25 +0530

    fixed WETH decimals

--- src/WETH.sol
+++ src/WETH.sol
@@ -5,7 +5,7 @@ import "solmate/tokens/ERC20.sol";
 
 contract WETH is ERC20{
 
-    constructor() ERC20("WETH", "WETH", 8) {}
+    constructor() ERC20("WETH", "WETH", 18) {}
 
     function mint(address recipient, uint256 amount) external {
         _mint(recipient, amount);
